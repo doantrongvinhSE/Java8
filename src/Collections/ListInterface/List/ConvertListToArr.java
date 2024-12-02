15793a85a95641ef7f318d34d3709a87e346731d
Đoàn Trọng Vinh <doantrongvinhse@gmail.com>
2024-12-02 11:31:50 +0700

    List Interface

--- src/Collections/ListInterface/List/ConvertListToArr.java
+++ src/Collections/ListInterface/List/ConvertListToArr.java
@@ -1,4 +1,4 @@
-package Collections.ListInterface;
+package Collections.ListInterface.List;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/Collections/ListInterface/List/ListWithAFactory.java
+++ src/Collections/ListInterface/List/ListWithAFactory.java
@@ -1,4 +1,4 @@
-package Collections.ListInterface;
+package Collections.ListInterface.List;
 
 import java.util.Arrays;
 import java.util.List;
