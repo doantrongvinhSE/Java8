30604eaa0aa039c86de3b607f72fad28e25d7eaf
Doan Trong Vinh <clonevinh95@gmail.com>
2024-12-10 10:35:29 +0700

    Stack

--- src/Collections/Stack/Main.java
+++ src/Collections/Stack/Main.java
@@ -1,4 +1,25 @@
 package Collections.Stack;
 
+import java.util.Stack;
+
 public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack);
+
+        System.out.println(stack.search(2)); // return 4 tinh tu 6
+
+
+
+    }
 }
